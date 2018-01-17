42c6206466669f1dc81253cf182e19e693f5d157
tom <thomasjohnmulroy@gmail.com>
2018-01-17 09:46:33 -0500

    finalize first part for submission

--- htmlParser.js
+++ htmlParser.js
@@ -1,32 +1,9 @@
 const cheerio = require('cheerio')
-// const lot1 = `<html>
-//     <title>
-//         Rembrandt Harmensz. van Rijn: Christ at Emmaus: The smaller Plate
-//     </title>
-//     <body>
-//       <h2>Rembrandt Harmensz. van Rijn</h2>
-//       <h3>Christ at Emmaus: The smaller Plate</h3>
-//         <div>
-//           Rembrandt Harmensz. van Rijn
-//           Christ at Emmaus: The smaller Plate
-//           etching with drypoint, 1634, without watermark, a good impression of this scarce print, New Hollstein's only state, beginning to show some wear in places, trimmed to or on the platemark, generally in very good condition
-//           P., S. 104 x 75 mm.
-//         </div>
-//       <h3>Price realised</h3>
-//       <div>GBP 6,875</div>
-//     </body>
-// </html>
-// `
-// const $ = cheerio.load(lot1)
-// let namesArray = []
-//
-// namesArray.push($('body').find('h2').text())
-//
-// console.log(`namesArray: ${namesArray}`);
 
-function parseHTML(htmlText) {
+const parseHTML = (htmlText) => {
   const $ = cheerio.load(htmlText)
-  return $('body').find('h2').text()
+  const name = $('body').find('h2').text()
+  return name
 }
 
 module.exports = parseHTML
--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const fs = require('fs')
 const htmlDirectory = './data/2015-03-18'
 const parseHTML = require('./htmlParser')
+const cheerio = require('cheerio')
 let namesArray = []
 
-fs.readdir(htmlDirectory, (err, htmlFiles) => {
-  if (err) {
-    console.error(`Error reading directory ${htmlDirectory}`)
-  }
-  htmlFiles.forEach((file, idx) => {
-    fs.readFile(`${htmlDirectory}/${file}`, 'utf8', (fileError, data) => {
-      if (fileError) throw fileError
-      namesArray.push(parseHTML(data))
-      console.log('Artist Names: ')
-      console.log(namesArray);
+// NOTE: need to change to async to allow namesArray to be logged once after completion
+  fs.readdir(htmlDirectory, (err, htmlFiles) => {
+    if (err) {
+      console.error(`Error reading directory ${htmlDirectory}`)
+    }
+
+
+    htmlFiles.forEach((file, idx) => {
+      fs.readFile(`${htmlDirectory}/${file}`, 'utf8', (fileError, data) => {
+        if (fileError) throw fileError
+        namesArray.push(parseHTML(data))
+        console.log('names array', namesArray);
+      })
     })
   })
-})
