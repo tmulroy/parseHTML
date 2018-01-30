d06eaf1369697c3a5e077ce01a9ea3a775983e35
tom <thomasjohnmulroy@gmail.com>
2018-01-30 13:15:53 -0500

    Extract artwork title and generate array of objects

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const readDir = promisify(fs.readdir);
 const cheerio = require('cheerio');
 const htmlDirectory = './data/2015-03-18';
 
+// TODO: account for unique artist names.
+
 const readFileAsync = async (fileName) => {
   const fileContents = await readFile(`${htmlDirectory}/${fileName}`, 'utf8');
   return fileContents
@@ -15,19 +17,30 @@ const getFileNamesAsync = async () => {
   return fileNames
 }
 
-const parseHTML = (htmlText) => {
+const getArtistNames = (htmlText) => {
   const $ = cheerio.load(htmlText);
   const name = $('body').find('h2').text();
   return name
 }
 
+const getTitle = (htmlText) => {
+  const $ = cheerio.load(htmlText);
+  const parsedTags = $('body').find('h3');
+  const title = parsedTags['0'].children[0].data;
+  return title
+}
+
 (async function () {
-  let namesArray = [];
+  let artworkArray = [];
   const htmlFileNames = await getFileNamesAsync();
   for (let file of htmlFileNames) {
+    let artworkObject = { artist: '', works: []}
     const fileContents = await readFileAsync(file);
-    let name = parseHTML(fileContents)
-    namesArray.push(name)
+    let name = getArtistNames(fileContents);
+    let title = getTitle(fileContents);
+    artworkObject.artist = name;
+    artworkObject.works.push(title)
+    artworkArray.push(artworkObject)
   }
-  console.log('Artist Names: \n', namesArray);
+  console.log('Artwork Artist Names and Works: \n', artworkArray);
 })()
